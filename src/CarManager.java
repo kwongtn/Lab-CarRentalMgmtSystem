fcf242af642486b42714c6828a6c76fa9d624147
KwongTN <tungnan5636@gmail.com>
2019-10-08 17:40:47 +0800

    Code Progress - Janson

--- src/CarManager.java
+++ src/CarManager.java
@@ -1,94 +1,116 @@
-public class CarManager{
-
-    private Car[] cars = new Car[5];
-    private int count;
-
-    public CarManager(int length){
-        cars = new Car[length];
+public class CarManager
+{
+    //private Car[] cars = new Car[5];
+ 
+    private Car[] cars;
+    private int count; 
+ 
+    public CarManager(int length)
+    {
+        cars=new Car[length];
     }
-
-
-    int addCar(Car car){
+ 
+    int addCar(Car car)
+    {
         int status = 0;
-
-        if(count < cars.length){
-            cars[count++] = car;
-            status = count;
+       
+        if (count<cars.length)
+        {
+            cars[count++]=car;         
+            //cars[counts]=car;
+            //count++;
+            status=count;
         }
-        
+               
         return status;
     }
-
-    int updateCar(Car car){
+ 
+    int updateCar(Car car)
+    {
         return 0;
     }
-
-    int deleteCar(Car car){
+   
+    int deleteCar(Car car)
+    {
         return 0;
     }
-    
-    Car[] searchByCapacity(int capacity){
-        return null;
-    }
-    
-    // Display all cars
-    void displayCars(){
-        for (Car car : cars){
-            if(car != null){
+   
+    void displayCar(int carID)
+    {
+        for (Car car : cars)//improved for loop
+        {
+            if (car!=null && car.getCarID()==carID)
+            {
                 displayCar(car);
             }
         }
     }
-    
-    void displayCar(int carID){
-        for (Car car : cars){
-            if(car != null && car.getCarID() == carID){
+   
+    void displayCars(double maxPrice)
+    {  
+        for (Car car : cars)//improved for loop
+        {
+            if (car!=null && car.getPrice()<=maxPrice)
+            {
+                displayCar(car);
+            }      
+        }
+    }
+ 
+    void displayCars(int minCapacity)
+    {
+        for (Car car : cars)//improved for loop
+        {
+            if (car!=null && car.getCapacity()>=minCapacity)
+            {
+                displayCar(car);
+            }      
+        }
+    }
+   
+   
+    void displayCars(boolean auto)
+    {
+        for (Car car : cars)//improved for loop
+        {
+            if (car!=null && car.isAuto()==auto)
+            {
+                displayCar(car);
+            }      
+        }
+    }
+   
+    void displayCars(String model)
+    {
+        for (Car car : cars)//improved for loop
+        {
+            if (car!=null && car.getModel().toLowerCase().contains(model.toLowerCase())) //toUpperCase()
+            {
+                displayCar(car);
+            }      
+        }
+    }
+ 
+ 
+    void displayCars()
+    {
+        for (Car car : cars)//improved for loop
+        {
+            if (car != null)
+            {
                 displayCar(car);
             }
         }
     }
-
-    void displayCars(double maxPrice){
-        for (Car car : cars){
-            if(car != null && car.getPrice() < maxPrice){
-                displayCar(car);
-            }
-        }
-    }
-
-    void displayCars(int minCapacity){
-        for (Car car : cars){
-            if(car != null && car.getCapacity() >= minCapacity){
-                displayCar(car);
-            }
-        }
-    }
-    
-    void displayCars(boolean transmissionSearch){
-        for (Car car : cars){
-            if(car != null && car.isAuto() == transmissionSearch){
-                displayCar(car);
-            }
-        }
-    }
-
-    void displayCars(String model){
-        for (Car car : cars){
-            if (car != null && car.getModel().toLowerCase().contains(model.toLowerCase())){
-                displayCar(car);
-            }
-        }
-    }
-
-    private void displayCar(Car car){
-        System.out.println("========================================");
+ 
+    private void displayCar(Car car)
+    {
         System.out.println("Car ID: " + car.getCarID());
-        System.out.println("Plate No.: " + car.getPlateNo());
-        System.out.println("Model:" + car.getModel());
-        System.out.println("Price: RM " + car.getPrice());
-        System.out.println("Is Auto:" + car.isAuto());
-        System.out.println("Is Usable:" + car.isUsable());
-        System.out.println("Capacity:" + car.getCapacity());
-        System.out.println();
+        System.out.println("Plate no.: " + car.getPlateNo());
+        System.out.println("Model: " + car.getModel());
+        System.out.println("Price: " + car.getPrice());
+        System.out.println("Is Auto: " + car.isAuto());
+        System.out.println("Is Usable: " + car.isUsable());
+        System.out.println("Capacity: " + car.getCapacity());
     }
 }
--- src/CarRentalManagementSystem.java
+++ src/CarRentalManagementSystem.java
@@ -1,53 +1,53 @@
-public class CarRentalManagementSystem{
-    public static void main(String args[]){
-        CarManager carManager = new CarManager(8);
-        
-
+public class CarRentalManagementSystem
+{
+    public static void main(String args[])
+    {
+        CarManager carManager = new CarManager(10);
+ 
         Car car1 = new Car(1001);
         car1.setPlateNo("ABC 1234");
         car1.setModel("Perodua Kancil");
+        car1.setPrice(5);
         car1.setAuto(false);
         car1.setUsable(true);
         car1.setCapacity(4);
-        car1.setPrice(5);
-
-        Car car2 = new Car(1003);
-        car2.setPlateNo("DEF 5678");
+ 
+        Car car2 = new Car(1002);
+        car1.setPlateNo("DEF 1234");
         car2.setModel("Perodua Alza");
+        car2.setPrice(10);
         car2.setAuto(true);
         car2.setUsable(true);
         car2.setCapacity(6);
-        car2.setPrice(10);
-
-        Car car3 = new Car(1005);
-        car3.setPlateNo("GHI 9012");
+ 
+        Car car3 = new Car(1003);
+        car3.setPlateNo("GHI 1123");
         car3.setModel("Perodua Myvi");
-        car3.setAuto(false);
-        car3.setUsable(true);
-        car3.setCapacity(4);
         car3.setPrice(8);
-
-        if (carManager.addCar(car1) != 0){
-            System.out.println("Car Added");
-        } else {
-            System.out.println("Car Cannot be Added.");
-        }
-        if (carManager.addCar(car2) != 0){
-            System.out.println("Car Added");
-        } else {
-            System.out.println("Car Cannot be Added.");
-        }
-        if (carManager.addCar(car3) != 0){
-            System.out.println("Car Added");
-        } else {
-            System.out.println("Car Cannot be Added.");
-        }
-
+        car3.setAuto(true);
+        car3.setUsable(false);
+        car3.setCapacity(5);
+ 
+        if (carManager.addCar(car1)!=0)
+            System.out.println("Car added");
+        else
+            System.out.println("Cannot add car");
+       
+        if (carManager.addCar(car2)!=0)
+            System.out.println("Car added");
+        else
+            System.out.println("Cannot add car");
+       
+        if (carManager.addCar(car3)!=0)
+            System.out.println("Car added");
+        else
+            System.out.println("Cannot add car");
+ 
         carManager.displayCars();
-        carManager.displayCars(1003);
-        carManager.displayCars(8D);
-        carManager.displayCars(4);
+        carManager.displayCar(1003);
+        carManager.displayCars(8.0);//8.0=8D
         carManager.displayCars(true);
-        carManager.displayCars("KANCIL");
+        carManager.displayCars("Kancil");
     }
+ 
 }
