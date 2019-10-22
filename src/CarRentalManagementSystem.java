937216a9cf5eba6ca4238d767038302ba9205caa
KwongTN <tungnan5636@gmail.com>
2019-10-22 17:06:47 +0800

    Rewrote CarRentalManagementSystem.main()
    - Added menu for action selection

--- src/CarRentalManagementSystem.java
+++ src/CarRentalManagementSystem.java
@@ -1,53 +1,45 @@
+
 public class CarRentalManagementSystem{
     public static void main(String args[]){
-        CarManager carManager = new CarManager(8);
-        
+        System.out.println("Welcome to Car Rental Management System");
+        System.out.println("YEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEET");
+        System.out.println("1 \t Manage Cars");
+        System.out.println("2 \t Manage Customers");
+        System.out.println("3 \t Manage Rentals");
+        System.out.println("0 \t Exit");
 
-        Car car1 = new Car(1001);
-        car1.setPlateNo("ABC 1234");
-        car1.setModel("Perodua Kancil");
-        car1.setAuto(false);
-        car1.setUsable(true);
-        car1.setCapacity(4);
-        car1.setPrice(5);
+        Scanner scanner = new Scanner(System.in);
+        int choice;
 
-        Car car2 = new Car(1003);
-        car2.setPlateNo("DEF 5678");
-        car2.setModel("Perodua Alza");
-        car2.setAuto(true);
-        car2.setUsable(true);
-        car2.setCapacity(6);
-        car2.setPrice(10);
+        do{
+            choice = -1;
+            while (choice < 0 || choice > 3){
+                System.out.print("Please enter an option: ");
+                choice = scanner.nextInt();
 
-        Car car3 = new Car(1005);
-        car3.setPlateNo("GHI 9012");
-        car3.setModel("Perodua Myvi");
-        car3.setAuto(false);
-        car3.setUsable(true);
-        car3.setCapacity(4);
-        car3.setPrice(8);
+                if(choice < 0 || choice > 3){
+                    System.out.println("Invalid option, please try again.");
+                }
+            }
 
-        if (carManager.addCar(car1) != 0){
-            System.out.println("Car Added");
-        } else {
-            System.out.println("Car Cannot be Added.");
-        }
-        if (carManager.addCar(car2) != 0){
-            System.out.println("Car Added");
-        } else {
-            System.out.println("Car Cannot be Added.");
-        }
-        if (carManager.addCar(car3) != 0){
-            System.out.println("Car Added");
-        } else {
-            System.out.println("Car Cannot be Added.");
-        }
+            switch (choice) {
+                case 1: {
+                    System.out.println("You've selected: Manage Cars");
+                    break;
+                }
 
-        carManager.displayCars();
-        carManager.displayCars(1003);
-        carManager.displayCars(8D);
-        carManager.displayCars(4);
-        carManager.displayCars(true);
-        carManager.displayCars("KANCIL");
+                case 2 : {
+                    System.out.println("You've selected: Manage Customers");
+                    break;
+                }
+
+                case 3 : {
+                    System.out.println("You're selected: Manage Rentals");
+                    break;
+                }
+            }
+
+
+        } while (choice != 4);
     }
 }
